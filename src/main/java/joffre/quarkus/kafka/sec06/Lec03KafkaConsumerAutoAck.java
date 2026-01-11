dd9e176cd4593e992195f283b6aa18e0b85d55b6
joffre <joffre.hermosilla@gmail.com>
2026-01-10 21:36:23 -0500

    septimo commit batch autoAck

---
+++ src/main/java/joffre/quarkus/kafka/sec06/Lec03KafkaConsumerAutoAck.java
@@ -0,0 +1,74 @@
+package joffre.quarkus.kafka.sec06;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+@ApplicationScoped
+public class Lec03KafkaConsumerAutoAck {
+
+    private static final Logger log =
+            LoggerFactory.getLogger(Lec03KafkaConsumerAutoAck.class);
+
+    private Disposable subscription;
+
+    void onStart(@Observes StartupEvent ev) {
+
+        var consumerConfig = Map.<String, Object>of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1",
+                ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 3
+        );
+
+        var options = ReceiverOptions.create(consumerConfig)
+                .commitInterval(Duration.ofSeconds(1))
+                .subscription(List.of("order-events"));
+
+        subscription = KafkaReceiver.create(options)
+                .receiveAutoAck()
+                .concatMap(this::batchProcess)
+                .doOnSubscribe(s ->
+                        log.info("Kafka consumer (auto-ack + batch) started"))
+                .doOnError(e ->
+                        log.error("Kafka consumer error", e))
+                .subscribe();
+    }
+
+    private Mono<Void> batchProcess(Flux<ConsumerRecord<Object, Object>> flux) {
+        return flux
+                .doFirst(() -> log.info("----------------"))
+                .doOnNext(r ->
+                        log.info("key: {}, value: {}", r.key(), r.value()))
+                .then(Mono.delay(Duration.ofSeconds(1)))
+                .then();
+    }
+
+    @PreDestroy
+    void shutdown() {
+        log.info("Shutting down Kafka consumer (auto-ack)");
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
+    }
+}
---
+++ src/main/java/joffre/quarkus/kafka/sec06/Lec03KafkaProducer.java
@@ -0,0 +1,67 @@
+package joffre.quarkus.kafka.sec06;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.annotation.PreDestroy;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+@ApplicationScoped
+public class Lec03KafkaProducer {
+    private static final Logger log =
+            LoggerFactory.getLogger(Lec03KafkaProducer.class);
+
+    private KafkaSender<String, String> sender;
+    private Disposable subscription;
+
+    void onStart(@Observes StartupEvent ev) {
+
+        var producerConfig = Map.<String, Object>of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        var options = SenderOptions.<String, String>create(producerConfig);
+        sender = KafkaSender.create(options);
+
+        var flux = Flux.range(1, 100)
+                .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+                .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        subscription = sender.send(flux)
+                .doOnSubscribe(s ->
+                        log.info("Kafka producer started"))
+                .doOnNext(r ->
+                        log.info("correlation id: {}", r.correlationMetadata()))
+                .doOnError(e ->
+                        log.error("Kafka producer error", e))
+                .doOnComplete(() ->
+                        log.info("Kafka producer completed"))
+                .subscribe();
+    }
+
+    @PreDestroy
+    void shutdown() {
+        log.info("Shutting down Kafka producer");
+        if (subscription != null && !subscription.isDisposed()) {
+            subscription.dispose();
+        }
+        if (sender != null) {
+            sender.close();
+        }
+    }
+}
